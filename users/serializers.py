193cb952324aba0b443cd800a48a7e6688d42016
Maxigong <maxi@nolgong.com>
2022-03-27 00:22:24 +0900

    add validators

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import NewUser
+from .models import NewUser as Users
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -8,13 +8,29 @@ class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
-        model = NewUser
+        model = Users
         fields = ('email', 'user_name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate_email(self, email):
+        existing = Users.objects.filter(email=email).first()
+        if existing:
+            raise serializers.ValidationError({"error": "email_error",
+                                               "messege": "This email address is already used by another account."},)
+        return email
+
+    def validate_user_name(self, user_name):
+        existing = Users.objects.filter(user_name=user_name).first()
+        if existing:
+            raise serializers.ValidationError({"error": "user_name_error",
+                                               "messege": "User name already exists, please choose another one."})
+        if len(user_name) < 3:
+            raise serializers.ValidationError({"error": "user_name_error",
+                                               "messege": "User name must be at least 3 characters long."})
+        return user_name
+
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        # as long as the fields are the same, we can just use this
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
