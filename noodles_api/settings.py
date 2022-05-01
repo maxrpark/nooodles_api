8e3de78ef82f8d33d327c953649e845cea6b2557
Maxigong <maxi@nolgong.com>
2022-05-01 14:07:56 +0900

    fix

--- noodles_api/settings.py
+++ noodles_api/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env('SECRET_KEY')
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
